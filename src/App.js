61c189b3da6c47c9492928f150fc1d10fd50850f
Ron Belkin <ronbel249@gmail.com>
2021-04-14 12:45:33 +0300

    Added components to app screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import React from 'react';
+import { useStoreContext } from './store';
+import {CurrencyTable, CurrencySelect} from './components';
 
 function App() {
+
+  const { data } = useStoreContext();
+
   return (
-    <div className="App">
- 
+    <div className="app">
+      <h1>Currency Exchange App</h1>
+      <h2>Base Currency: {data.baseCurrency}</h2>
+      
+      <CurrencySelect />
+
+      <CurrencyTable />
     </div>
   );
 }
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import CurrencySelect from './currency-select/currency-select';
+import CurrencyTable from './currency-table/currency-table';
+
+export {CurrencyTable, CurrencySelect};
